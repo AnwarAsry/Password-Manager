07661d9f592b0101ae21bd466f2b3add19db89a5
AnwarAsry <anwareducation03@gmail.com>
2024-12-07 13:09:50 +0100

    Added new font and layout on landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
+import { Roboto } from "next/font/google"
 
 export const metadata: Metadata = {
 	title: "Password manager",
 	description: "A Password Management Website that securely stores and manages login credentials, featuring Google authentication, encrypted storage, and a user-friendly dashboard for adding, searching, and editing accounts.",
 };
+
+const roboto = Roboto({
+	weight: '400',
+	subsets: ['latin'],
+})
 
 export default function RootLayout({
 	children,
@@ -15,7 +21,7 @@ export default function RootLayout({
 	return (
 		// SessionProvider necessary for useSession hook in client components 
 		<SessionProvider>
-			<html lang="en">
+			<html lang="en" className={roboto.className}>
 				<body>
 					{children}
 				</body>
