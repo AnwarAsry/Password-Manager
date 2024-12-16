414dfbec79a72d5486da6576019e260ee201dac5
AnwarAsry <anwareducation03@gmail.com>
2024-12-16 23:54:01 +0100

    html for table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { getAllCredentialsForUser } from "@/actions/account";
 import { auth } from "@/auth";
+import { Tag } from "@/components/Tag";
 import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
@@ -9,12 +10,33 @@ const Dashboard = async () => {
         redirect("/")
     }
 
-    const data = await getAllCredentialsForUser(session.user.id)
-    console.log(data);
+    const res = await getAllCredentialsForUser(session.user.id)
 
     if (session.user) {
         return <>
             <h1>Helloo Welcome!!!</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Platform</th>
+                        <th>Category</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        res.data?.map(obj => {
+                            return <tr key={obj.id}>
+                                <td>{obj.platform}</td>
+                                <td>{obj.category?.map((tag, i) => <Tag key={i} text={tag} />)}</td>
+                                <td>{obj.username}</td>
+                                <td>{obj.password}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
         </>
     }
 }
