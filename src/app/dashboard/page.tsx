c70b16a88ce89a1b59c5285e41a0ac2287cbdd52
AnwarAsry <anwareducation03@gmail.com>
2024-12-17 19:57:01 +0100

    thead designed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { getAllCredentialsForUser } from "@/actions/account";
 import { auth } from "@/auth";
 import { Tag } from "@/components/Tag";
 import { redirect } from "next/navigation";
+import tableStyles from "@/styles/CredentialsTable.module.scss"
 
 const Dashboard = async () => {
     const session = await auth();
@@ -15,28 +16,32 @@ const Dashboard = async () => {
     if (session.user) {
         return <>
             <h1>Helloo Welcome!!!</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Platform</th>
-                        <th>Category</th>
-                        <th>Username</th>
-                        <th>Password</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        res.data?.map(obj => {
-                            return <tr key={obj.id}>
-                                <td>{obj.platform}</td>
-                                <td>{obj.category?.map((tag, i) => <Tag key={i} text={tag} />)}</td>
-                                <td>{obj.username}</td>
-                                <td>{obj.password}</td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </table>
+            <section>
+                <table className={tableStyles.tableCredentials} >
+                    <thead>
+                        <tr>
+                            <th>Platform</th>
+                            <th>Category</th>
+                            <th>Username</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            res.data?.map(obj => {
+                                return <tr key={obj.id}>
+                                    <td>
+                                        {obj.platform}
+                                    </td>
+                                    <td>{obj.category?.map((tag, i) => <Tag key={i} text={tag} />)}</td>
+                                    <td>{obj.username}</td>
+                                    <td>{obj.password}</td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </section>
         </>
     }
 }
