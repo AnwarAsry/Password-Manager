608f9eb47bc5e939e06878994a58fd6ca12dbcff
AnwarAsry <anwareducation03@gmail.com>
2024-12-20 13:44:27 +0100

    Done btn design

--- src/components/CredentialsAddBtn.tsx
+++ src/components/CredentialsAddBtn.tsx
@@ -2,13 +2,14 @@
 
 import { useState } from "react";
 import { CredentialsForm } from "./CredentialsForm";
+import ButtonStyles from "@/styles/Buttons.module.scss";
 
 export const CredentialsAddBtn = () => {
 
     const [showForm, setShowForm] = useState(false);
 
     return <>
-        <button onClick={() => setShowForm(true)}>Add Credentials</button>
+        <button className={ButtonStyles.PrimaryBtn} onClick={() => setShowForm(true)}>Add Credentials</button>
 
         {showForm && <CredentialsForm Cancel={() => setShowForm(false)} />}
     </>
