f361d6c87947fb53f707ac0ae91abd5942f5146c
AnwarAsry <anwareducation03@gmail.com>
2024-12-23 14:20:57 +0100

    Page structure for credential done

--- src/app/(dashboard)/view/[id]/page.tsx
+++ src/app/(dashboard)/view/[id]/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { getCredential } from "@/actions/account";
+import { Tag } from "@/components/Tag";
 import { IAccounts } from "@/models/IAccounts";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { platform } from "os";
 import { useEffect, useState } from "react";
 
 const CredentialPage = () => {
@@ -28,7 +30,29 @@ const CredentialPage = () => {
     return <>
         <Link href="/dashboard">Back to homepage</Link>
         <h3>{pageInfo?.platform}</h3>
+        <div>
+            <label>
+                Username/Email:
+                <input type="text" disabled defaultValue={pageInfo?.username} />
+            </label>
+            <label>
+                Password:
+                <input type="password" disabled defaultValue={pageInfo?.password} />
+            </label>
+        </div>
+        <label>
+            Notes:
+            <textarea disabled defaultValue={pageInfo?.notes}></textarea>
+        </label>
 
+        <div>
+            <label>Taggs:</label>
+            {
+                pageInfo?.category?.map((obj, i) => {
+                    return <Tag key={i} text={obj} />
+                })
+            }
+        </div>
     </>
 }
 
