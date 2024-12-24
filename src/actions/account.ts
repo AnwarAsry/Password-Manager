cb9bc34e24cb05abc0c9c90491c512093c70e3c2
AnwarAsry <anwareducation03@gmail.com>
2024-12-24 21:33:17 +0100

    decryption done

--- src/actions/account.ts
+++ src/actions/account.ts
@@ -3,11 +3,11 @@ import { ServerAction, ServerActionResponse } from "@/models/responses/ServerAct
 import { get, post, remove } from "@/serviceBase";
 
 // Fetch all Credentials saved by user
-export const getAllCredentialsForUser = async (userId: string): Promise<ServerActionResponse<IAccounts[] | null>>  	 => {
+export const getAllCredentialsForUser = async (userId: string): Promise<ServerActionResponse<IAccounts[] | null>> => {
     try {
         // Send the userId as parameter
         const response = await get<ServerActionResponse<IAccounts[]>>(`/credential/${userId}`)
-
+        
         // Check if the Server did not succeed in fetching
         if (!response.success) {
             // Return unsuccessfull message
@@ -64,6 +64,7 @@ export const createAccount = async (prevState, formData: FormData): Promise<Serv
     }
 };
 
+// Fetching the data based on the searchText
 export const getSearchResult = async (searchText: string): Promise<ServerActionResponse<IAccounts[] | null>> => {
     try {
         // Send the text as query
@@ -83,6 +84,7 @@ export const getSearchResult = async (searchText: string): Promise<ServerActionR
     }
 }
 
+// Get the saved Credential by id
 export const getCredential = async (id: string): Promise<ServerActionResponse<IAccounts | null>> => {
     try {
         // Send the id as param
@@ -102,6 +104,7 @@ export const getCredential = async (id: string): Promise<ServerActionResponse<IA
     }
 }
 
+// Delete request for deleting a credential based on id
 export const deleteCredential = async (id: string): Promise<ServerAction> => {
     try {
         // Send the id as param
