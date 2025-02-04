6b40c05211945269a1a738c6d8e11fe0a8d90f9a
AnwarAsry <anwareducation03@gmail.com>
2025-02-04 17:36:27 +0100

    created navbar structure

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+    return <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Bookmark</li>
+                <li>Settings</li>
+            </ul>
+        </nav>
+    </>
+}
