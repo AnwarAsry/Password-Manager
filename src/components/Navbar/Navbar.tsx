db716abe717ebbbeb8f3eabf6d3c1856ee06c483
AnwarAsry <anwareducation03@gmail.com>
2025-02-08 11:15:41 +0100

    added icons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import NavbarStyles from "@/styles/Navbar.module.scss"
 
 import Link from "next/link"
+import { IoBookmarkOutline, IoHomeOutline, IoSettingsOutline } from "react-icons/io5"
 
 
 export const Navbar = () => {
@@ -8,12 +9,15 @@ export const Navbar = () => {
         <nav className={NavbarStyles.Navbar}>
             <div className={NavbarStyles.NavList}>
                 <Link href="/dashboard" className={NavbarStyles.NavItem}>
+                    <IoHomeOutline className={NavbarStyles.NavIcons} />
                     Home
                 </Link>
                 <Link href="/bookmark" className={NavbarStyles.NavItem}>
+                    <IoBookmarkOutline className={NavbarStyles.NavIcons} />
                     Bookmark
                 </Link>
                 <Link href="/settings" className={NavbarStyles.NavItem}>
+                    <IoSettingsOutline className={NavbarStyles.NavIcons} />
                     Settings
                 </Link>
             </div>
