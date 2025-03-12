4db0a850fa04883bc37a07e1fdb916706ba7e578
AnwarAsry <anwareducation03@gmail.com>
2025-03-12 11:53:26 +0100

    done

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import TableStyles from "@/styles/CredentialsTable.module.scss";
 import TagStyles from "@/styles/Tag.module.scss";
-import HeaderStyles from "@/styles/Header.module.scss";
 
 import { getAllCredentialsForUser } from "@/actions/account";
 import { validateSession } from "@/utils/ValidateSession";
 
-import { SearchBar } from "@/components/Search/SearchBar";
 import { CredentialsTable } from "@/components/Table/CredentialsTable";
 import { CredentialsAddBtn } from "@/components/CredentialsAddBtn";
 import { TableRow } from "@/components/Table/TableRow";
@@ -17,9 +15,6 @@ const Dashboard = async () => {
     const res = await getAllCredentialsForUser(session.user.id)
 
     return <>
-        {/* <header className={HeaderStyles.HeaderInMain}>
-            <SearchBar />
-        </header>
         <section>
             <div className={TableStyles.TitleCallToAction}>
                 <h5 className={TableStyles.Title}>
@@ -32,7 +27,7 @@ const Dashboard = async () => {
             <CredentialsTable>
                 {res.data?.map(item => <TableRow key={item.id} entity={item} />)}
             </CredentialsTable>
-        </section> */}
+        </section>
     </>
 }
 
