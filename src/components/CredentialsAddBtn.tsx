2e5422482b6edba3717cc0238eb54ee5d09f6613
AnwarAsry <anwareducation03@gmail.com>
2025-04-09 23:09:14 +0200

    added func for show form and style changes

--- src/components/CredentialsAddBtn.tsx
+++ src/components/CredentialsAddBtn.tsx
@@ -1,6 +1,9 @@
-import ButtonStyles from "@/styles/Buttons.module.scss";
+"use client"
 
+import ButtonStyles from "@/styles/Buttons.module.scss";
 import { HiPlus } from "react-icons/hi";
+import { CredentialsForm } from "./Forms/CredentialsForm";
+import { useState } from "react";
 
 interface ICredentialsAddBtnProps {
     text?: string
@@ -8,10 +11,14 @@ interface ICredentialsAddBtnProps {
 }
 
 export const CredentialsAddBtn = ({ text, addIcon }: ICredentialsAddBtnProps) => {
+
+    const [showForm, setShowForm] = useState(false);
+
     return <>
-        <button className={ButtonStyles.PrimaryBtn}>
+        <button className={ButtonStyles.PrimaryBtn} onClick={() => setShowForm(true)}>
             {addIcon && <HiPlus className={ButtonStyles.BtnIcon} />}
             {text}
         </button>
+        {showForm && <CredentialsForm Cancel={() => setShowForm(false)} />}
     </>
 }
