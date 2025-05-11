dac9ba102c9ece51e65d429cd028630111b0a05e
AnwarAsry <anwareducation03@gmail.com>
2025-05-11 19:08:13 +0200

    doen

--- src/components/User/UserCard.tsx
+++ src/components/User/UserCard.tsx
@@ -3,23 +3,44 @@
 import UserCardStyles from '@styles/UserCard.module.scss'
 import avatar from '@public/avatar.png'
 import Image from 'next/image'
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
+import { useState } from 'react'
+import { TbLogout2, TbSettings } from 'react-icons/tb'
 
 export const UserCard = () => {
     const { data: session } = useSession();
 
+    const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
     // Checks if there is a session and has a user
     if (session && session.user) {
         return (
-            <div className={UserCardStyles.UserCard}>
-                <Image className={UserCardStyles.UserCardPic} src={session.user.image!} alt="Profile" width={40} height={40} />
+            <div className={UserCardStyles.User}>
+                <div className={UserCardStyles.UserCard} onClick={() => setShowDropdown(!showDropdown)}>
+                    <Image className={UserCardStyles.UserCardPic} src={session.user.image!} alt="Profile" width={32} height={32} />
+                    <p>{session.user.name}</p>
+                </div>
+                {
+                    showDropdown && (
+                        <ul className={UserCardStyles.UserDropdown}>
+                            <li>
+                                <TbSettings />
+                                Account Settings
+                            </li>
+                            <li onClick={async () => await signOut()}>
+                                <TbLogout2 />
+                                Log out
+                            </li>
+                        </ul>
+                    )
+                }
             </div>
         )
     }
 
     return (
         <div className={UserCardStyles.UserCard}>
-            <Image className={UserCardStyles.UserCardPic} src={avatar} alt="default pic" width={40} height={40} />
+            <Image className={UserCardStyles.UserCardPic} src={avatar} alt="default pic" width={32} height={32} />
         </div>
     )
 }
